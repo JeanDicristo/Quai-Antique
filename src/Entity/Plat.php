3ea7f8a220b51b9ce34c24465ae87dae71306563
jean di cristo <jean.dicristo@hotmail.Com>
2023-01-26 18:02:28 +0100

    Validation entity plat

--- src/Entity/Plat.php
+++ src/Entity/Plat.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\PlatRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PlatRepository::class)]
 class Plat
@@ -15,13 +16,20 @@ class Plat
     private ?int $id = null;
 
     #[ORM\Column(length: 70)]
+    #[Assert\Length(min: 0, max: 70)]
+    #[Assert\NotBlank()]
     private ?string $name = null;
 
-    #[ORM\Column]
+    #[ORM\Column(type: Types::FLOAT)]
+    #[Assert\NotNull()]
+    #[Assert\Positive()]
     private ?float $price = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Image = null;
 
     public function getId(): ?int
     {
@@ -60,6 +68,18 @@ class Plat
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(string $Image): self
+    {
+        $this->Image = $Image;
 
         return $this;
     }
